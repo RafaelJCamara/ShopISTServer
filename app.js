46a394951d0ef200b7f30d51ee0695ef33af43fa
rafaeljcamara <rafaelcamarac@gmail.com>
2021-03-22 23:22:26 +0000

    Add password encryption.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const dbUrl = "mongodb://localhost:27017/shopist";
 const db = mongoose.connection;
 const User = require("./models/user");
-
+const bcrypt = require("bcrypt");
 
 //connect to database
 
@@ -27,19 +27,33 @@ db.once("open", () => {
 //using json
 app.use(express.json());
 
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
+
+
 //ROUTES
 app.post("/signup", async (req, res) => {
     console.log("**********************************");
     console.log("There was a signup request");
     console.log(req.body);
     console.log("**********************************");
-
-    const newUser = new User(req.body);
-    await newUser.save();
-
-    res.status(200).send();
+    //get user info
+    const { username, email, password } = req.body;
+    try {
+        const hashedPassword = await hashPassword(password);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(200).send();
+    } catch (e) {
+        console.log("Error: ", e);
+    }
 
 });
+
+
 
 app.post("/login", async (req, res) => {
     console.log("**********************************");
@@ -48,16 +62,16 @@ app.post("/login", async (req, res) => {
     console.log("**********************************");
 
     const foundUser = await User.find({ email: req.body.email });
-    console.log(foundUser[0].password);
-    console.log(req.body.password);
-    const sendUser = {
-        name: foundUser[0].username,
-        email: foundUser[0].email
-    }
 
-    if (req.body.password == foundUser[0].password) {
+    const comparePw = await bcrypt.compare(req.body.password, foundUser[0].password);
+
+    if (comparePw) {
         //matching passwords
         console.log("Matching passwords");
+        const sendUser = {
+            name: foundUser[0].username,
+            email: foundUser[0].email
+        }
         return res.status(200).send(JSON.stringify(sendUser));
     } else {
         //no matching passwords
